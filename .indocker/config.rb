b96a32777e3c06731fce2ff03543b6d5531554ef
vocrsz <vocrsz@gmail.com>
2017-11-02 20:05:31 +0500

    fixes to rdm

--- .indocker/config.rb
+++
@@ -1,5 +0,0 @@
-Indocker.setup do
-  load_env_file %w(env_files/planiro.dev.env)
-end
-
-require_relative 'items'
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+gem install bundler
+
+bundle config --global --jobs 8 && bundle install
+
 cd /app && rake db:setup && rake db:migrate
 
 exec "$@"
