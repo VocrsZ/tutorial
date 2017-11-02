d0dd11970af1ece3e8d4b934a6e99370b2aa7aa5
vocrsz <vocrsz@gmail.com>
2017-11-02 22:41:26 +0500

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+bundle
+
 gem install bundler
 
 bundle config --global --jobs 8 && bundle install
 
-cd /app && rake db:setup && rake db:migrate
+cd /app && bundle exec rake db:setup && bundle exec rake db:migrate
 
 exec "$@"
